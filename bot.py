b4ef04b8798adb5744035b0d04ef3e21746909cb
MikolaKz <qazmik23@mail.ru>
2020-04-16 23:15:44 +0600

    добавил отправку фото

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters 
+from glob import glob
+from random import choice
 import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 import settings
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -14,10 +18,15 @@ def start(update,context):
     update.message.reply_text(text)
 
 
+def send_nail_design(update,context):
+    naiil_list=glob("images/nogt*.jp*g")
+    nail_pic=choice(naiil_list) 
+    update.message.bot.send_photo(update.message.chat_id,open(nail_pic,'rb'))
+
+
 def talk_to_me(update,context):
     cur_message=update.message
     userText = "Привет {}! Ты написал: {}".format(cur_message.chat.first_name,cur_message.text)
-    print(update.message)
     update.message.reply_text(userText)
 
 
@@ -26,6 +35,8 @@ def main():
    
    dp=mybot.dispatcher
    dp.add_handler(CommandHandler('start',start))
+   dp.add_handler(CommandHandler('nail',send_nail_design))
+
    dp.add_handler(MessageHandler(Filters.text,talk_to_me))
    
    mybot.start_polling()
