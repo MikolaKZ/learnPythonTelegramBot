37edcab2db285d17b9cb710cec58bf361eb409a2
MikolaKz <qazmik23@mail.ru>
2020-04-21 22:48:56 +0600

    Добавил клавиатуру контакты и локация и вынес в отдельную функцию

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
+from emoji import emojize
 from glob import glob
 from random import choice
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup, KeyboardButton
 
 import settings
 
@@ -13,10 +15,26 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
 
 
 def start(update,context):
-    text="Вызван старт"
-    logging.info(text)
-    update.message.reply_text(text)
+    
+    emo=emojize(choice(settings.USER_EMOJI),use_aliases=True)
+    greatEmo=emojize(choice(settings.GREAT_EMOJI),use_aliases=True)
+    context.user_data["emo"]=greatEmo
+    chatInfo=getChatInfo(update)
+    
+    contact_button = KeyboardButton('Прислать контакты', request_contact=True)
+    location_button = KeyboardButton('Прислать координаты', request_location=True)
 
+
+    text="Привет {}! Меня зовут мастер Биба!{}".format(chatInfo.first_name,greatEmo)
+
+    my_keyboard=ReplyKeyboardMarkup([
+                                        ['Мои работы','Сменить аватарку'],
+                                        [contact_button,location_button]
+                                    ])
+    update.message.reply_text(text,reply_markup=my_keyboard)
+
+def getChatInfo(update):
+    return update.message.chat
 
 def send_nail_design(update,context):
     naiil_list=glob("images/nogt*.jp*g")
@@ -26,25 +44,45 @@ def send_nail_design(update,context):
 
 def talk_to_me(update,context):
     cur_message=update.message
-    userText = "Привет {}! Ты написал: {}".format(cur_message.chat.first_name,cur_message.text)
+    userText = "{}Привет {}! Ты написал: {}".format(context.user_data.get("emo"),cur_message.chat.first_name,cur_message.text)
     update.message.reply_text(userText)
 
 
-def main():
-   mybot = Updater(settings.API_KEY,use_context=True)
-   
-   dp=mybot.dispatcher
-   dp.add_handler(CommandHandler('start',start))
-   dp.add_handler(CommandHandler('nail',send_nail_design))
+def get_contact(update,context):
+    print (update.message.contact)
 
-   dp.add_handler(MessageHandler(Filters.text,talk_to_me))
+
+def change_avatar(update,context):
+    greatEmoOld = None
+    if 'emo' in context.user_data:
+        greatEmoOld = context.user_data['emo']
+        del context.user_data['emo']
+    greatEmo=emojize(choice(settings.GREAT_EMOJI),use_aliases=True)
+    context.user_data["emo"]=greatEmo
+    if greatEmoOld is None:
+        update.message.reply_text("New avatar added = {}".format(greatEmo))
+    else:
+        update.message.reply_text("Avatar has been changed from {} to {}".format(greatEmoOld,greatEmo))
+    
+
+def main():
+    mybot = Updater(settings.API_KEY,use_context=True)
    
-   mybot.start_polling()
-   logging.info("Бот запущен")
-   mybot.idle()
+    dp=mybot.dispatcher
+    dp.add_handler(CommandHandler('start',start,pass_user_data=True))
+    dp.add_handler(CommandHandler('nail',send_nail_design,pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^(Мои работы)$'),send_nail_design,pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.regex('^(Сменить аватарку)$'),change_avatar,pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.contact, get_contact, pass_user_data=True))
+    
+    dp.add_handler(MessageHandler(Filters.text,talk_to_me,pass_user_data=True))
+
+    mybot.start_polling()
+    mybot.idle()
+
 
 
 main()
-
-
     
+
+
